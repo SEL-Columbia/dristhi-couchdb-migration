62fc7fb28439a7cf6c9ce953b93c7d6dabb65313
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-01-20 17:42:19 +0530

    Thiru & Kiran | #808 | Wrapped async API with promise.

--- anc_visits_migartion.js
+++ anc_visits_migartion.js
@@ -1,27 +1,35 @@
 var _ = require('underscore')._;
+var q = require('q');
 var cradle = require('cradle');
-var ancs = require('./ancs.js')
-var ancVisits = [];
 var FORM_DB_NAME = 'drishti-form';
 var DRISTHI_DB_NAME = 'drishti';
 var DB_SERVER = 'http://localhost';
 var DB_PORT = 5984;
+var dristhiFormDb;
+var dristhiDb;
 
-function ANCVisit(entityId, ancVisitNumber, ancVisitDate, weight, bpSystolic, bpDiastolic) {
-    'use strict';
-    var self = this;
-    self.entityId = entityId;
-    self.ancVisitNumber = ancVisitNumber;
-    self.ancVisitDate = ancVisitDate;
-    self.weight = weight;
-    self.bpSystolic = bpSystolic;
-    self.bpDiastolic = bpDiastolic;
-}
+var init = function () {
+    var deferred = q.defer();
+    deferred.resolve();
+    return deferred.promise;
+};
 
-var dristhiFormDb = new cradle.Connection(DB_SERVER, DB_PORT, {cache: true, raw: false}).database(FORM_DB_NAME);
-var dristhiDb = new cradle.Connection(DB_SERVER, DB_PORT, {    cache: true, raw: false}).database(DRISTHI_DB_NAME);
+var connectToDristhiFormDB = function () {
+    var deferred = q.defer();
+    dristhiFormDb = new cradle.Connection(DB_SERVER, DB_PORT, {cache: true, raw: false}).database(FORM_DB_NAME);
+    deferred.resolve();
+    return deferred.promise;
+};
 
-var createAllFormSubmissionsByNameAndEntityIDView = function (dristhiFormDb) {
+var connectToDristhiDB = function () {
+    var deferred = q.defer();
+    dristhiDb = new cradle.Connection(DB_SERVER, DB_PORT, {    cache: true, raw: false}).database(DRISTHI_DB_NAME);
+    deferred.resolve();
+    return deferred.promise;
+};
+
+var createAllFormSubmissionsByNameAndEntityIDView = function () {
+    var deferred = q.defer();
     dristhiFormDb.save('_design/FormSubmission_Temp', {
         views: {
             byFormNameAndEntityId: {
@@ -32,11 +40,19 @@ var createAllFormSubmissionsByNameAndEntityIDView = function (dristhiFormDb) {
                 }
             }
         }
+    }, function (err, res) {
+        if (err) {
+            console.log('Error when creating Form Submission by name and entity id view. Message: ' + err);
+            deferred.reject(err);
+        }
+        deferred.resolve(res);
     });
+    return deferred.promise;
 };
 
-var createAllOpenANCsView = function (drishtiDb) {
-    drishtiDb.save('_design/Mother_Temp', {
+var createAllOpenANCsView = function () {
+    var deferred = q.defer();
+    dristhiDb.save('_design/Mother_Temp', {
         views: {
             allOpenANCs: {
                 map: function (doc) {
@@ -46,10 +62,71 @@ var createAllOpenANCsView = function (drishtiDb) {
                 }
             }
         }
+    }, function (err, res) {
+        if (err) {
+            console.log('Error when creating All Open ANCs view. Message: ' + err);
+            deferred.reject(err);
+        }
+        deferred.resolve(res);
+    });
+    return deferred.promise;
+};
+
+var updateANCWithANCVisitInformation = function (response) {
+    var ancVisitFormSubmissions = response.ancVisitFormSubmissions;
+    var openANCs = response.openANCs;
+
+    var ancVisits = _.map(ancVisitFormSubmissions, function (r) {
+        var fields = _.filter(r.doc.formInstance.form.fields, function (field) {
+            return field.name === 'ancVisitDate' ||
+                field.name === 'weight' ||
+                field.name === 'bpSystolic' ||
+                field.name === 'bpDiastolic';
+        });
+        return {
+            entityId: r.doc.entityId,
+            ancVisitDate: _.find(fields,function (field) {
+                return field.name === 'ancVisitDate';
+            }).value,
+            weight: _.find(fields,function (field) {
+                return field.name === 'weight';
+            }).value,
+            bpSystolic: _.find(fields,function (field) {
+                return field.name === 'bpSystolic';
+            }).value,
+            bpDiastolic: _.find(fields,function (field) {
+                return field.name === 'bpDiastolic';
+            }).value
+        }
+    });
+//            console.log(JSON.stringify(ancVisits));
+    _.each(openANCs, function (anc) {
+        var allANCVisitsForMother = _.where(ancVisits, {entityId: anc.value});
+        _.each(allANCVisitsForMother, function (ancVisit) {
+            delete ancVisit.entityId;
+        });
+        anc.key.ancVisits = allANCVisitsForMother;
+    });
+    console.log("Updated ANCs: " + JSON.stringify(openANCs));
+    var deferred = q.defer();
+    deferred.resolve(openANCs);
+    return deferred.promise;
+};
+
+var updateMotherDocument = function (openANCs) {
+    _.each(openANCs, function (anc) {
+        dristhiDb.merge(anc.id, anc.key, function (err, res) {
+            if (err) {
+                console.log('Could not update mother due to error. Error: ' + JSON.stringify(err) + ', Mother: ' + anc.value);
+            } else {
+                console.log('Updated mother: ' + anc.value);
+            }
+        });
     });
 };
 
-var getAllANCVisitsForANC = function (dristhiFormDb, dristhiDb, openANCs) {
+var getAllANCVisitsForANC = function (openANCs) {
+    var deferred = q.defer();
     var entityIds = _.pluck(openANCs, 'value');
     console.log('Found ' + entityIds.length + ' open ANCs.');
 
@@ -63,82 +140,35 @@ var getAllANCVisitsForANC = function (dristhiFormDb, dristhiDb, openANCs) {
         function (err, res) {
             if (err) {
                 console.log('Error when getting ANC visit forms: ' + JSON.stringify(err));
+                deferred.reject(err);
                 return;
             }
             console.log('Found ' + res.length + ' ANC Visits.');
-
-            var ancVisits = _.map(res, function (r) {
-                var fields = _.filter(r.doc.formInstance.form.fields, function (field) {
-                    return field.name === 'ancVisitDate' ||
-                        field.name === 'weight' ||
-                        field.name === 'bpSystolic' ||
-                        field.name === 'bpDiastolic';
-                });
-                return {
-                    entityId: r.doc.entityId,
-                    ancVisitDate: _.find(fields,function (field) {
-                        return field.name === 'ancVisitDate';
-                    }).value,
-                    weight: _.find(fields,function (field) {
-                        return field.name === 'weight';
-                    }).value,
-                    bpSystolic: _.find(fields,function (field) {
-                        return field.name === 'bpSystolic';
-                    }).value,
-                    bpDiastolic: _.find(fields,function (field) {
-                        return field.name === 'bpDiastolic';
-                    }).value
-                }
-            });
-//            console.log(JSON.stringify(ancVisits));
-            _.each(openANCs, function (anc) {
-                var allANCVisitsForMother = _.where(ancVisits, {entityId: anc.value});
-                _.each(allANCVisitsForMother, function (ancVisit) {
-                    delete ancVisit.entityId;
-                });
-                anc.key.ancVisits = allANCVisitsForMother;
-            });
-            console.log("Updated ANCs: " + JSON.stringify(openANCs));
-            _.each(openANCs, function (anc) {
-                dristhiDb.merge(anc.id, anc.key, function (err, res) {
-                    if (err) {
-                        console.log('Could not update mother due to error. Error: ' + JSON.stringify(err) + ', Mother: ' + anc.value);
-                    } else {
-                        console.log('Updated mother: ' + anc.value);
-                    }
-                });
-            });
+            deferred.resolve({ancVisitFormSubmissions: res, openANCs: openANCs});
         });
+
+    return deferred.promise;
 };
 
-var getAllOpenANCs = function (dristhiDb) {
-    return dristhiDb.view('Mother_Temp/allOpenANCs', function (err, response) {
+var getAllOpenANCs = function () {
+    var deferred = q.defer();
+    dristhiDb.view('Mother_Temp/allOpenANCs', function (err, response) {
         if (err) {
             console.log('Error when finding open ANCs: ' + JSON.stringify(err));
-            return;
+            deferred.reject(err);
         }
-//        console.log("Mothers: " + JSON.stringify(response));
-//        console.log("EntityIds: " + entityIds);
-        getAllANCVisitsForANC(dristhiFormDb, dristhiDb, response);
+        deferred.resolve(response);
     });
+    return deferred.promise;
 };
 
-
-createAllFormSubmissionsByNameAndEntityIDView(dristhiFormDb);
-createAllOpenANCsView(dristhiDb);
-getAllOpenANCs(dristhiDb);
-
-
-//requiredANCs.forEach(function(anc) {
-//    anc.key.ancVisits = _.filter(ancVisits, function (ancVisit) {
-//        return anc.key.caseId === ancVisit.entityId;
-//    });
-//    anc.key.ancVisits.forEach(function (ancVisit){ delete ancVisit.entityId; });
-//});
-
-
-
-
-
-
-
+init()
+    .then(connectToDristhiFormDB)
+    .then(connectToDristhiDB)
+    .then(createAllFormSubmissionsByNameAndEntityIDView)
+    .then(createAllOpenANCsView)
+    .then(getAllOpenANCs)
+    .then(getAllANCVisitsForANC)
+    .then(updateANCWithANCVisitInformation)
+    .then(updateMotherDocument)
+;
