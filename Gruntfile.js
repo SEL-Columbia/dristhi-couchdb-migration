b8613814b0a4167a7cbcac44984a02e3aeb73939
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-08-21 13:53:14 +0530

    Thiru | #1042 | The users can be deleted from the database

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,11 @@
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('grunt-prompt');
     var target = grunt.option('target') || '0_help_file';
+    var username = grunt.option('username') || {};
+    var environment = grunt.option('env') || 'dev';
+    var port = grunt.option('port') || '5984';
 
     grunt.initConfig({
         jshint: {
@@ -25,31 +29,42 @@ module.exports = function (grunt) {
                     watch: ''
                 }
             },
-            dev: {
-                script: 'lib/index.js',
-                options: {
-                    args: ['http://localhost', 5984, 'drishti-form', 'drishti', target],
-                    watch: ''
-                }
-            },
-            ec2: {
-                script: 'lib/index.js',
-                options: {
-                    args: ['http://localhost', 5985, 'drishti-form', 'drishti', target],
-                    watch: ''
-                }
-            },
             prod: {
                 script: 'lib/index.js',
                 options: {
-                    args: ['http://localhost', 5986, 'drishti-form', 'drishti', target],
+                    args: ['http://localhost', port, 'drishti-form', 'drishti', target, username],
                     watch: ''
                 }
             }
+        },
+        prompt: {
+            migration: {
+                options: {
+                    questions: [
+                        {
+                            config: 'prompt',
+                            type: 'input',
+                            message: 'Are you sure that you want to delete the ' + JSON.stringify(username) + ' user?',
+                            default: 'no'
+                        }
+                    ],
+                    then: function () {
+                        var config = grunt.config('prompt');
+                        if ((config.toLowerCase() === 'y' || config.toLowerCase() === 'yes') && JSON.stringify(username) !== '{}')
+                            grunt.task.run('nodemon:prod');
+                        return true;
+                    }
+                },
+                script: 'lib/index.js'
+            }
         }
     });
+
     grunt.registerTask('default', ['help']);
     grunt.registerTask('build', ['jshint']);
     grunt.registerTask('migrate-dev', ['nodemon:dev']);
     grunt.registerTask('help', ['nodemon:help']);
+    grunt.registerTask('delete-user', function () {
+        grunt.task.run('prompt:migration');
+    });
 };
